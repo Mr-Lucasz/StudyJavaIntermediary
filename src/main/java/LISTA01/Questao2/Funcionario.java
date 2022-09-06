c14da333b78ee9add44f329fba05a4ad56dd8b4b
Lucas Rodrigues <l.cunha14.lc@gmail.com>
2022-09-05 21:12:30 -0300

    swing

--- src/main/java/LISTA01/Questao2/Funcionario.java
+++ src/main/java/LISTA01/Questao2/Funcionario.java
@@ -8,6 +8,29 @@ package LISTA01.Questao2;
  *
  * @author 01295230208
  */
-public class Q2Funcionarios {
+public class Funcionario {
+
+    static Funcionario copiarFuncionario(Funcionario funcionario4) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String nome;
+    public String codigo;
+    int geradorCodigo;
+    
+    Funcionario(String name) {
+    this.nome = nome;
+    this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+   
     
 }
