fdf16c2762acf3e0e2c6fd4cbb7779d362945bbb
Lucas Rodrigues <l.cunha14.lc@gmail.com>
2022-10-03 22:10:12 -0300

    teste

--- src/main/java/collection/Listas/AgendaMap.java
+++ src/main/java/collection/Listas/AgendaMap.java
@@ -4,10 +4,43 @@
  */
 package collection.Listas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author 01295230208
  */
-public class AgendaMap {
+public class AgendaMap implements Agenda {
+    private Map<String, Contato> contato;
+    
+    public AgendaMap(){
+        contato = new HashMap<>();
+        
+    }
+
+    @Override 
+    public void addContato(Contato novoContato){
+         contato.put(novoContato.getTelefone(), novoContato);
+    }
+    
+    @Override 
+    public  Contato buscarContatoNome(String nome){
+        for(String telefone : contato.keySet()){
+            Contato c = contato.get(telefone);
+            if(c.getNome().equals(nome))
+          return c;
+                }
+        return null;
+           
+        
+    }
+    @Override 
+    public Contato buscarContatoTelefone(String telefone){
+      return contato.get(telefone);
+    }
+    
+    
+    
     
 }
--- src/main/java/collection/Listas/AgendaSet.java
+++ src/main/java/collection/Listas/AgendaSet.java
@@ -4,10 +4,44 @@
  */
 package collection.Listas;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author 01295230208
  */
-public class AgendaSet {
+public class AgendaSet implements Agenda{
+    private Set<Contato> contatos;
+    //evitar a duplica
+
+    public AgendaSet() {
+        contatos = new HashSet<>();
+    }
+    
+    
+    @Override 
+    public void addContato(Contato contato){
+         contatos.add(contato);  
+    }
+    
+    @Override 
+    public  Contato buscarContatoNome(String nome){
+     for(Contato c: contatos){
+         if(c.getNome().equals(nome))
+         return c;
+     }
+        return null;
+        
+    }
+    @Override 
+    public Contato buscarContatoTelefone(String telefone){
+         for(Contato c: contatos){
+         if(c.getTelefone().equals(telefone))
+         return c;
+     }
+        return null;    
+    }
+    
     
 }
